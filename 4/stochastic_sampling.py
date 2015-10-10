84b21e66d2e1914f207077b7653e6677fe57ff14
RyanKim01 <kyhekm@hotmail.com>
2015-10-10 10:16:34 -0700

    Finished version of stochastic_sampling

--- 4/stochastic_sampling.py
+++ 4/stochastic_sampling.py
@@ -1,12 +1,19 @@
 import sys
 import random
+from collections import defaultdict
 
 
-
+def histogram(textfile):
+    d = defaultdict(int)
+    for word in textfile:
+        d[word] += 1
+    return d
 
 
 if __name__ == '__main__':
     argv = sys.argv[1]
     with open(argv, 'r') as words:
         collection_of_words = words.read().split()
-    print(random.choice(collection_of_words))
+    print("Picked word with weight is", random.choice(collection_of_words))
+    print("Randomly picked word is",
+          random.choice(list(histogram(collection_of_words).keys())))
